6acd7346ed1a94758899dd19929d8ab89b2d7806
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-02-23 16:20:52 +0200

    uplode js

---
+++ JS/app.js
@@ -0,0 +1,33 @@
+var gender='girl';
+var grade='seventh';
+var mathMark=20;
+var nationality='Jordanian';
+var genderUserInput=prompt("provied us of your ginder?");
+var gradeUserInput=prompt("provied us of your grade?");
+var mathMarkUserInput=prompt("provied us of your mathMark?");
+var nationalityUserInput=prompt("provied us of your nationality?");
+if (genderUserInput==='girl' && gradeUserInput==='seventh' && mathMarkUserInput >=20 && nationalityUserInput==='Jordanian'){
+    console.log("that is correct");
+    alert('congratulation')
+}else{console.log("that is not correct");
+alert('Good luck')
+};
+// if (){
+//     console.log("that is correct");
+//     alert('congratulation')
+// }else{console.log("that is correct");
+// alert('Good luck')
+// };
+// if (){
+//     console.log("that is correct");
+//     alert('congratulation')
+// }else{console.log("that is correct");
+// alert('Good luck')
+// };
+// if (){
+//     console.log("that is correct");
+//     alert('congratulation')
+// }else{console.log("that is correct");
+// alert('Good luck')
+// };
+
